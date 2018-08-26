b25dee738fed75a8abc7fecea4055cb31a3f663c
56 <sherryqueenlove@gmail.com>
2018-08-26 21:22:56 +0800

    添加 menu 展开与收缩事件

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import 'normalize.css'
 import './index.scss'
 import avatar from './assets/avatar.png'
 
-const dom = document.getElementById('avatar')
-dom.src = avatar
+// 显示头像
+const $dom = document.getElementById('js-avatar')
+$dom.src = avatar
+
+// 绑定交互事件
+const $menuLayout = document.getElementById('js-menu-layout')
+const $menu = document.getElementById('js-menu')
+$menu.addEventListener('click', () => $menuLayout.classList.toggle('expand'))
