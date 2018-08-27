7d80034612214eafd7741b05a6c0901dc3b8f657
56 <sherry@ibustv.com>
2018-08-27 20:27:22 +0800

    避免首屏空白

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import 'normalize.css'
 import './index.scss'
 import avatar from './assets/avatar.png'
 
-// 显示头像
-const $dom = document.getElementById('js-avatar')
-$dom.src = avatar
+document.addEventListener('DOMContentLoaded', () => {
+  // 显示头像
+  const $dom = document.getElementById('js-avatar')
+  $dom.src = avatar
 
-// 绑定交互事件
-const $menuLayout = document.getElementById('js-menu-layout')
-const $menu = document.getElementById('js-menu')
-$menu.addEventListener('click', () => $menuLayout.classList.toggle('expand'))
+  // 绑定交互事件
+  const $menuLayout = document.getElementById('js-menu-layout')
+  const $menu = document.getElementById('js-menu')
+  $menu.addEventListener('click', () => $menuLayout.classList.toggle('expand'))
+
+  document.body.style.display = 'flex'
+})
